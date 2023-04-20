4bd9378e6be35f0f15d6e2c69d1a18e5e2b56a30
Dmitry <gix73@yandex.ru>
2023-04-20 23:15:02 +0300

    feat: add webpack config

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Test } from "./styled";
+
+const App = () => {
+  return (
+    <Test>
+      <div>Hello</div>
+    </Test>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const production = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: { myAppName: path.resolve(__dirname, "./src/index.js") },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: production ? "[name].[contenthash].js" : "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          production ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: !production,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack & React",
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+    new MiniCssExtractPlugin({
+      filename: production ? "[name].[contenthash].css" : "[name].css",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  mode: production ? "production" : "development",
+};
