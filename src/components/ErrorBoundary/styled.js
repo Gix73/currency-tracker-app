a17ec2058720c5c16c89c7eff00f872f204d08c6
Dmitry <gix73@yandex.ru>
2023-05-11 13:39:25 +0300

    feat: add ErrorBoundary

---
+++ src/components/ErrorBoundary/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ErrorHeader = styled.h1`
+  color: red;
+`;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Global } from "./globalStyled.js";
+import ErrorBoundary from "@components/ErrorBoundary/ErrorBoundary.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Global />
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>
 );
 
