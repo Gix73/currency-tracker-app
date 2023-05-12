1f942dae96ecb338886b9825f92ed5ef69f256b5
Dmitry <gix73@yandex.ru>
2023-05-12 18:28:35 +0300

    feat: add lazy and loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "@components/Footer/Footer";
 import Header from "@components/Header/Header";
 import { AppWrapper, Container } from "./styled";
-import HomePage from "@pages/HomePage/HomePage";
-import { useEffect, useRef, useState } from "react";
+import { Suspense, lazy, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import ModalConverter from "@components/Modal/ModalConverter";
-import TimelinePage from "@pages/TimelinePage/TimelinePage";
 import { exchangeValues } from "@constants/exchangeValues";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@constants/themeData";
-import BankCardPage from "@pages/BankCardPage/BankCardPage";
-import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import Loading from "@components/Loading/Loading";
+
+const HomePage = lazy(() => import("@pages/HomePage/HomePage"));
+const TimelinePage = lazy(() => import("@pages/TimelinePage/TimelinePage"));
+const BankCardPage = lazy(() => import("@pages/BankCardPage/BankCardPage"));
+const ContactsPage = lazy(() => import("@pages/ContactsPage/ContactsPage"));
 
 const App = () => {
   const [currencyData, setCurrencyData] = useState({});
@@ -37,18 +39,18 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_${chartCurrency}_USD/latest?period_id=1MTH`,
-        {
-          headers: {
-            "X-CoinAPI-Key": process.env.REACT_APP_COINAPI_KEY,
-          },
-        }
-      )
-      .then((res) => {
-        setCandleData(() => res.data);
-      });
+    // axios
+    //   .get(
+    //     `https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_${chartCurrency}_USD/latest?period_id=1MTH`,
+    //     {
+    //       headers: {
+    //         "X-CoinAPI-Key": process.env.REACT_APP_COINAPI_KEY,
+    //       },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     setCandleData(() => res.data);
+    //   });
     setLastUpdateTime(new Date().toLocaleTimeString());
   }, [chartCurrency]);
 
@@ -63,36 +65,37 @@ const App = () => {
       <AppWrapper>
         <Container>
           <Header toggleTheme={setThemeColor} lastUpdate={lastUpdateTime} />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <HomePage
-                  data={currencyData}
-                  onShow={setShowModal}
-                  onExchange={exchangeHandler}
-                  exchangeValues={exchangeValues}
-                />
-              }
-            />
-            <Route
-              path="/timeline"
-              element={
-                <TimelinePage
-                  candleData={candleData}
-                  onCurrencyChange={setChartCurrency}
-                  chartCurrency={chartCurrency}
-                  exchangeValues={exchangeValues}
-                />
-              }
-            />
-            <Route
-              path="/bankcard"
-              element={<BankCardPage exchangeValues={exchangeValues} />}
-            />
-            <Route path="/contacts" element={<ContactsPage />} />
-          </Routes>
-
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <HomePage
+                    data={currencyData}
+                    onShow={setShowModal}
+                    onExchange={exchangeHandler}
+                    exchangeValues={exchangeValues}
+                  />
+                }
+              />
+              <Route
+                path="/timeline"
+                element={
+                  <TimelinePage
+                    candleData={candleData}
+                    onCurrencyChange={setChartCurrency}
+                    chartCurrency={chartCurrency}
+                    exchangeValues={exchangeValues}
+                  />
+                }
+              />
+              <Route
+                path="/bankcard"
+                element={<BankCardPage exchangeValues={exchangeValues} />}
+              />
+              <Route path="/contacts" element={<ContactsPage />} />
+            </Routes>
+          </Suspense>
           <Footer />
         </Container>
         {
